93bb83b9b03aafb8a5d3a79e9d0c27bd5fe2e96f
17s11 <2503850133@qq.com>
2025-08-02 22:44:04 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 1000,
+        height: 700,
+        webPreferences: {
+            nodeIntegration: false,
+            contextIsolation: true,
+        }
+    });
+
+    // 加载本地 HTML 文件
+    win.loadFile(path.join(__dirname, 'app', 'index.html'));
+}
+
+app.whenReady().then(() => {
+    createWindow();
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow();
+    });
+});
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit();
+});
